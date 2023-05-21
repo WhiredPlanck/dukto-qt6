8608c67b28c41c1f3d9dace28b2689e75ad06f24
WhiredPlanck <whiredplanck@outlook.com>
2023-05-21 23:31:36 +0800

    Update Chinese Simplified translations

---
+++ i18n/dukto-qt6_zh_CN.ts
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>Created by Emanuele Colombo</source>
+        <translation>由 Emanuele Colombo 创建</translation>
+    </message>
+    <message>
+        <source>Website: &lt;a href=&quot;http://www.msec.it/dukto/&quot;&gt;http://www.msec.it/dukto/&lt;/a&gt;</source>
+        <translation>网站：&lt;a href=&quot;http://www.msec.it/dukto/&quot;&gt;http://www.msec.it/dukto/&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <source>This application and it&apos;s source code are released freely as open source project.&lt;br&gt;If you like this application please consider &lt;a href=&quot;http://www.msec.it/&quot;&gt;making a donation&lt;/a&gt;.</source>
+        <translation>本应用及其源码已经以开源项目的形式自由发布。&lt;br&gt;如果您喜欢这个应用请考虑&lt;a href=&quot;http://www.msec.it/&quot;&gt;捐献&lt;/a&gt;。</translation>
+    </message>
+</context>
+<context>
+    <name>BuddyListItemModel</name>
+    <message>
+        <source> (You)</source>
+        <translation>（您）</translation>
+    </message>
+    <message>
+        <source>IP</source>
+        <translation>IP</translation>
+    </message>
+    <message>
+        <source>IP connection</source>
+        <translation>IP 连接</translation>
+    </message>
+    <message>
+        <source>Send data to a remote device</source>
+        <translation>发送数据给远程设备</translation>
+    </message>
+</context>
+<context>
+    <name>IPPage</name>
+    <message>
+        <source>Your IP addresses</source>
+        <translation>您的 IP 地址</translation>
+    </message>
+</context>
+<context>
+    <name>RecentPage</name>
+    <message>
+        <source>Sorry, no data has been
+received yet...</source>
+        <translation>抱歉，还没有接收到任何数据……</translation>
+    </message>
+    <message numerus="yes">
+        <source>from %1, %2</source>
+        <translation>
+            <numerusform>发自 %1，%2</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>TabBar</name>
+    <message>
+        <source>buddies</source>
+        <translation>伙伴</translation>
+    </message>
+    <message>
+        <source>recent</source>
+        <translation>最近</translation>
+    </message>
+    <message>
+        <source>about</source>
+        <translation>关于</translation>
+    </message>
+</context>
+<context>
+    <name>ToolBar</name>
+    <message>
+        <source>Received</source>
+        <translation>已接收</translation>
+    </message>
+    <message>
+        <source>Addresses</source>
+        <translation>地址</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>Dukto</source>
+        <translation>Dukto</translation>
+    </message>
+</context>
+</TS>
